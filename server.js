05b7b8ae63ce7697e7e40820309e1ffcf85212fb
Glitch (hello-express) <none>
2019-04-15 09:18:30 +0000

    😜📹 Checkpoint ./server.js:343052/2004

--- server.js
+++ server.js
@@ -13,8 +13,34 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
+  // TODO: Check cookie
+  // TODO: If user is not signed in, show `/views/index.html` with id/password form.
+  // TODO: If user is signed in, redirect to `/reauth`.
+  // TODO: `/views/index.html` shows id/password field along with "sign in" button.
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.post('/login', function(request, response) {
+  // TODO: Accept sign-in as long as `id` present. Ignore `password`
+  // TODO: If sign-in failed, return 401.
+  // TODO: If sign-in succeeded, redirect to `/home`.
+});
+
+app.get('/home', function(request, response) {
+  // TODO: If user is not signed in, redirect to `/`.
+  // TODO: If user is signed in, redirect to `/reauth`.
+  // TODO: `/views/home.html` shows sign-out link
+  response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/logout', function(request, response) {
+  response.sendFile(__dirname + '/views/logout.html');
+});
+
+app.get('/reauth', function(request, response) {
+  response.sendFile(__dirname + '/views/reauth.html');
+});
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
