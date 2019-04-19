6476ee51a3ed265c43c3cf136eecbf86e0943d25
Glitch (hello-express) <none>
2019-04-19 07:19:14 +0000

    👐👁 Checkpoint ./libs/auth.js:343052/808

--- libs/auth.js
+++ libs/auth.js
@@ -14,14 +14,15 @@ db.defaults({
 
 /**
  * Checks CSRF protection using custom header `X-Requested-With`
- * If cookie doesn't contain `id`, consider 
+ * If cookie doesn't contain `id`, consider the user is not authenticated.
  **/
 const sessionCheck = (req, res, next) => {
   if (req.header('X-Requested-With') != 'XMLHttpRequest') {
     res.status(400).json({error: 'invalid access.'});
+    return;
   }
   if (!req.cookies.id) {
-    res.status(400).json({error: 'not signed in.'});
+    res.status(401).json({error: 'not signed in.'});
     return;
   }
   next();
@@ -35,6 +36,7 @@ const sessionCheck = (req, res, next) => {
 router.post('/signin', upload.array(), (req, res) => {
   if (req.header('X-Requested-With') != 'XMLHttpRequest') {
     res.status(400).json({error: 'invalid access.'});
+    return;
   }
   // If cookie doesn't contain an id, let in as long as `id` present (Ignore password)
   if (!req.body.id) {
@@ -46,12 +48,14 @@ router.post('/signin', upload.array(), (req, res) => {
     res.cookie('id', req.body.id);
     res.status(200).json({});
   }
+  return;
 });
 
 // For test purposes
 router.post('/putKey', upload.array(), sessionCheck, (req, res) => {
   if (!req.body.credential) {
     res.status(400).json({ error: 'invalid request' });
+    return;
   }
   const stab = {
     id: req.cookies.id,
@@ -91,7 +95,7 @@ router.post('/removeKey', upload.array(), sessionCheck, (req, res) => {
 });
 
 /**
- * Fetch required information before calling navigator.credential.create()
+ * Respond with required information to call navigator.credential.create()
  * Input is passed via `req.body` with similar format as output
  * Output format:
  * ```{
@@ -125,13 +129,36 @@ router.post('/removeKey', upload.array(), sessionCheck, (req, res) => {
 router.post('/makeCred', upload.array(), sessionCheck, (req, res) => {
 });
 
+/**
+ * Register user credential.
+ **/
 router.post('/regCred', upload.array(), sessionCheck, (req, res) => {
+  // Replace this `stab` with proper credential info to store
+  const stab = {
+    id: req.cookies.id,
+    credential: req.body.credential
+  };
+  // Store user info
+  db.get('users')
+    .push(stab)
+    .write();
+  // Respond with user info
+  res.json(stab);
 });
 
 router.post('/getAsst', upload.array(), sessionCheck, (req, res) => {
 });
 
 router.post('/authAsst', upload.array(), sessionCheck, (req, res) => {
+  // Query the user
+  const user = db.get('users')
+    .find({ id: req.cookies.id })
+    .value();
+
+  // TODO: Verify the signature against public key included in the user info
+
+  // Respond with user info if verified. Otherwise error.
+  res.json(user || {});
 });
 
 module.exports = router;
