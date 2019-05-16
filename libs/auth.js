c1731b78ebe953c36e5d4715d4d6d369e5a3ea93
Glitch (webauthn-codelab-resolution) <none>
2019-05-16 13:32:35 +0000

    👡🚜 Checkpoint ./views/home.html:343052/7476 ./views/index.html:343052/4694 ./views/reauth.html:343052/5016 ./server.js:343052/5403 ./libs/auth.js:343052/25870 ./public/style.css:343052/1533 ./public/client.js:343052/7716

--- libs/auth.js
+++ libs/auth.js
@@ -1,3 +1,19 @@
+/*
+ * @license
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
@@ -5,8 +21,17 @@ const { Fido2Lib } = require('fido2-lib');
 const { coerceToBase64Url,
         coerceToArrayBuffer
       } = require('fido2-lib/lib/utils');
+const fs = require('fs');
 
 const low = require('lowdb');
+
+try {
+  fs.readFileSync('./.data/db.json');
+} catch (e) {
+  fs.mkdirSync('./.data');
+  fs.writeFileSync('./.data/db.json', '');
+}
+
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('.data/db.json');
 const db = low(adapter);
