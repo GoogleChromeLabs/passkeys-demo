a40a3c3ab9bb07ce026add32e8bd00fe45764a01
Glitch (webauthn-codelab) <none>
2022-05-25 04:38:12 +0000

    🦄🍢 Checkpoint ./public/components.js:343052/43 ./views/home.html:343052/800 ./public/style.scss:343052/391

--- public/components.js
+++ public/components.js
@@ -3,5 +3,6 @@ import '@material/mwc-fab';
 import '@material/mwc-top-app-bar-fixed';
 import '@material/mwc-textfield';
 import '@material/mwc-list';
+import '@material/mwc-icon';
 import '@material/mwc-icon-button';
 import '@material/mwc-linear-progress';
