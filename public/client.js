49c1307a2df2ad527ebbef38e399668efa2090ad
Glitch (passkey-form-demo) <none>
2023-02-20 06:11:27 +0000

    🚟😋 Checkpoint ./public/client.js:343052/16

--- public/client.js
+++ public/client.js
@@ -40,13 +40,13 @@ export async function _fetch(path, payload = '') {
   }
 };
 
-const base64url = {
+export const base64url = {
   encode: function(buffer) {
     return atob(buffer).replace(/\+/, '-').replace(/\//, '_').replace(/=+$/, '');      
   },
   decode: function(base64url) {
     const base64 = base64url.replace(/-/, '+').replace(/_/, '/');
-    return btoa(base64url).
+    return btoa(base64);
   }
 }
 
